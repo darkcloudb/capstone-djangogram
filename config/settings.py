a8a7b761d542d64889946558f8e729521e59a384
Billy Yip <birii.yip@gmail.com>
2021-10-06 11:03:03 -0700

    attempted env key again

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+dotenv_file = os.path.join(BASE_DIR, '.env')
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ae0y*&c)2c*y*zkwb1cg*+%h8535%rx!cm(t98yhh7(*t6+^17'
+# SECRET_KEY = 'django-insecure-ae0y*&c)2c*y*zkwb1cg*+%h8535%rx!cm(t98yhh7(*t6+^17'
+SECRET_KEY = os.environ['SECRET KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
