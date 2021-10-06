a46f6e770ce2084b4e56544076915f6d389cd411
Billy Yip <birii.yip@gmail.com>
2021-10-06 10:57:12 -0700

    Extracted secrets from the settings file

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,11 @@
+import environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
+
 """
 Django settings for config project.
 
@@ -21,10 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ae0y*&c)2c*y*zkwb1cg*+%h8535%rx!cm(t98yhh7(*t6+^17'
+# SECRET_KEY = 'django-insecure-ae0y*&c)2c*y*zkwb1cg*+%h8535%rx!cm(t98yhh7(*t6+^17'
+SECRET_KEY=env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -81,10 +91,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
+        'default': env.db(),
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
